a96cf4a738806e41196e5fd012a4725f38cfc44d
yanakin394 <yana.germatskaya@gmail.com>
2023-04-03 11:17:40 +0300

    constants created

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ const secondPurchase = +document.getElementById('secondItem').textContent;
 const thirdPurchase = +document.getElementById('thirdItem').textContent;
 const fourthPurchase = +document.getElementById('fourthItem').textContent;
 
-const sum = (firstPurchase, secondPurchase, thirdPurchase, fourthPurchase) => {
+const sum = () => {
     return firstPurchase + secondPurchase + thirdPurchase + fourthPurchase;
 
 }
-console.log(sum);
+const totalSum = sum(firstPurchase, secondPurchase, thirdPurchase, fourthPurchase);
+console.log(totalSum);
 
 const total = document.getElementById('totalValue');
-total.textContent = sum;
+total.textContent = totalSum + ' руб.';
 
-const discountSum = (sum) => {
-    return sum * 0.8;
+const discountTotalSum = () => {
+    return totalSum * 0.8;
 }
+discountTotalSum(totalSum);
+console.log(discountTotalSum); //не отображается в консоли!
 
 const applyDiscount = document.getElementById('discountButton');
-applyDiscount.addEventListener("click", discountSum => {
-    total.textContent = discountSum;
+applyDiscount.addEventListener("click", discountTotalSum => {
+    total.textContent = discountTotalSum + ' руб.';
 })
-
-console.log(discountSum);
